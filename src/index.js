5f3227fb2ee686f1731660095dc1ae057532843f
Simona Limontaite <simonachess@gmail.com>
2021-09-22 09:24:45 +0300

    add footer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import Footer from "./Footer";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <Footer />
   </StrictMode>,
   rootElement
 );
