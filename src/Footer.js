469b7d01c32760a3bb244f254daae402722443b2
Simona Limontaite <simonachess@gmail.com>
2021-09-22 09:24:33 +0300

    add footer

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div>
+            <small/>Coded by Simona Limontaite <a href="https://github.com/simonachess">Github</a>
+        </div>
+    );
+}
